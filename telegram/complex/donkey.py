90b16203da0a4df09c40f5ebedba6213978f5bad
Galina <116437191+Galina-Basargina@users.noreply.github.com>
2023-10-24 17:04:39 +0300

    Настроила прием данных

--- telegram/complex/donkey.py
+++ telegram/complex/donkey.py
@@ -1,7 +1,6 @@
 import json
 import requests
 import telebot
-from time import sleep
 
 drone_service = '127.0.0.1'
 token = ""
@@ -24,14 +23,18 @@ def run(command):
 def send_welcome(message):
     global delivery_price
     global weight_price
+    global street_marker
     weight_price = None
     delivery_price = None
+    street_marker = None
+
 
     bot.reply_to(message,
 """Здравствуй!
 Сколько будет весить твой груз? 1, 2 или 3 кг?
 Ты можешь заказать доставку на ул. Ватутина, ул.Садовая, Витебский пр. и Невский пр.
-Будь внимателен - напиши в точности так же как написано в списке!"""
+Будь внимателен - напиши в точности так же как написано в списке!
+Ты можешь отменить заказ написав 'Отмена заказа'"""
    )
 
 @bot.message_handler(commands=['status'])
@@ -43,7 +46,8 @@ def send_status(message):
 def echo_all(message):
     global delivery_price
     global weight_price
-
+    global street_marker
+    
     text = ''
     if message.text == "1":
         text = "Ты заказал доставку на 1 кг\n"
@@ -71,6 +75,13 @@ def echo_all(message):
         text = "Ты выбрал адрес Невский пр.\n"
         street_marker = 4
         delivery_price = 800
+    
+    elif message.text == "Отмена заказа":
+            weight_price = None
+            delivery_price = None
+            street_marker = None
+            bot.reply_to(message, "Вы отменили свой заказ")
+            return
 
     else:
         bot.reply_to(message, "Прости, но я не понял что ты написал. Проверь свое сообщение")
@@ -83,16 +94,23 @@ def echo_all(message):
         bot.reply_to(message, text+"Ты не выбрал адрес доставки, для продолжения требуется его указать")
         return
     bot.reply_to(message, text+"Стоимость доставки: "+str(delivery_price+weight_price))
-    
+
     response = run({'do': 'Move', 'street': street_marker})
     text = ''
     if response.get('error'):
         text += 'Ошибка: '+str(response.get('error'))
+        street_marker = None
     else:
         if response.get('speed'):
-            text += 'Дрон двигается со скоростью '+str(response.get('speed'))+"м.\n"
+            text += 'Дрон двигается со скоростью '+str(response.get('speed'))+"м/с.\n"
         if response.get('altitude'):
             text += 'Дрон находится на высоте '+str(response.get('altitude'))+"м.\n"
+            print(message.text)
+            street_marker = None
+            weight_price = None
+            delivery_price = None
+        message.text = ""
+
     bot.reply_to(message, text)
 
 if __name__ == "__main__":
